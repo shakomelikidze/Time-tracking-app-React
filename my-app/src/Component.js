1e692d40658379423ca033a3470aab14b66432ba
shakomelikidze <shako456@gmail.com>
2023-11-10 16:19:10 +0400

    final

--- my-app/src/Component.js
+++ my-app/src/Component.js
@@ -1,37 +1,48 @@
-import React from 'react'
-import dots from './images/ellipsis.svg'
+import React from 'react';
+import dots from './images/ellipsis.svg';
 
 const Component = (props) => {
-  const {activity, image, className, dailyValue, weeklyValue, monthlyValue, activeButton} = props;
+  const { 
+    activity, image, className, 
+    dailyValue, weeklyValue, monthlyValue, 
+    activeButton 
+  } = props;
+
+  const getTimeValue = () => {
+    if (activeButton === 'daily') {
+      return dailyValue !== 0 ? `${dailyValue}Hrs` : '0 Hrs';
+    } else if (activeButton === 'weekly') {
+      return weeklyValue !== 0 ? `${weeklyValue}Hrs` : '0 Hrs';
+    } else if (activeButton === 'monthly') {
+      return monthlyValue !== 0 ? `${monthlyValue}Hrs` : '0 Hrs';
+    }
+    return '0 Hrs'; // Default to '0 Hrs' if activeButton is not recognized
+  };
+
   return (
     <>
       <div className="activity-card">
         {/* coloured side of card */}
-        <div className={props.className}>
-          <img src={image}></img>
+        <div className={className}>
+          <img src={image} alt={activity} />
         </div>
         {/* info in the card */}
         <div className="data-card">
-          <div class="card-title">
-            <p class="card-title-p">{activity}</p>
-            <button class="three-dot-btn">
-              <img src={dots}></img>
+          <div className="card-title">
+            <p className="card-title-p">{activity}</p>
+            <button className="three-dot-btn">
+              <img src={dots} alt="Three dots" />
             </button>
           </div>
           {/* time div */}
-          <div class="card-info">
-            <p className="current-time current-time-work">
-              {activeButton === 'daily' && dailyValue}
-              {activeButton === 'weekly' && weeklyValue}
-              {activeButton === 'monthly' && monthlyValue}
-              Hrs
-            </p>
+          <div className="card-info">
+            <p className="current-time current-time-work">{getTimeValue()}</p>
           </div>
-          {/* info after click on three dot button */}
+          {/* info after clicking on the three-dot button */}
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Component
+export default Component;
